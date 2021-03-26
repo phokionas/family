ab281524a93aaf284406f193a242c81faf81f72e
phokionas <79746893+phokionas@users.noreply.github.com>
2021-03-26 12:11:22 +0000

    Deploying to gh-pages from master @ 2bfc9210af20ff6f0a029cf97be42de0af26850c 🚀

---
+++ buch-outdoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="buch-outdoor" tilewidth="32" tileheight="32" tilecount="72" columns="12">
+ <image source="../../../../../Program Files/tiled/examples/buch-outdoor.png" width="384" height="192"/>
+</tileset>
---
+++ texts.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="texts" tilewidth="32" tileheight="32" tilecount="121" columns="11">
+ <image source="texts.png" width="352" height="352"/>
+</tileset>
